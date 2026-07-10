c3944d640b12b3b0aec6901d487ba58c97a7526a
MarcialTiburcio <91584181+MarcialTiburcio@users.noreply.github.com>
2026-07-09 22:17:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
 st.sidebar.title("Parámetros")
 
-st.image("Image20260709220548.png")
+st.image("DMC.png")
 
 valor_inicial = st.number_input("Ingrese el valor inicial", value=0)
 valor_final = st.number_input("Ingrese el valor final", value=1)
